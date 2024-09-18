53110fa93cb8ee31b0b04868290203aea5ed01d7
ishiko <ishiko732@gmail.com>
2024-09-18 22:07:50 +0800

    add retrievability

--- src/tsFsrsCalculator.ts
+++ src/tsFsrsCalculator.ts
@@ -16,10 +16,13 @@ export class TsFsrsCalculator {
     }
 
     public steps(reviews: number[]): Card[] {
-        let fsrs_card = createEmptyCard(new Date());
+        const day = 24 * 60 * 60 * 1000;
+        const first_date = new Date();
+        let fsrs_card = createEmptyCard(first_date);
 
-        let card = new Card(0, 0.0, 0.0, 0.0, 0.0, 0.0, 0);
+        let card = new Card(0, 0.0, 0.0, 0.0, 0.0, 0.0, 0, first_date);
         const list = [];
+        let last_date = first_date;
 
         const f = fsrs(generatorParameters({
             w: this.w,
@@ -32,7 +35,7 @@ export class TsFsrsCalculator {
             fsrs_card = f.next(fsrs_card, date, <Grade>review, (recordItem) => {
                 const { card } = recordItem;
                 const interval = f.next_interval(card.stability, card.elapsed_days)
-                card.due = new Date(date.getTime() + interval * 24 * 60 * 60 * 1000);
+                card.due = new Date(date.getTime() + interval * day);
                 card.scheduled_days = interval;
                 return card
             });
@@ -41,9 +44,14 @@ export class TsFsrsCalculator {
             const interval = fsrs_card.scheduled_days;
             const cumulativeInterval = card.cumulativeInterval + interval;
             card.cumulativeInterval = cumulativeInterval;
-            list.push(new Card(fsrs_card.state, fsrs_card.difficulty, displayDifficulty, fsrs_card.stability, interval, cumulativeInterval, review));
+            const next_card = new Card(fsrs_card.state, fsrs_card.difficulty, displayDifficulty, fsrs_card.stability, interval, cumulativeInterval, review, fsrs_card.last_review!)
+            last_date = fsrs_card.due
+            list.push(next_card);
+            for (const item of list) {
+                const retrievability = f.forgetting_curve((last_date.getTime() - item.last_review.getTime()) / day, next_card.stability) * 100;
+                item.retrievability.push(retrievability);
+            }
         }
-
         return list;
     }
 }
@@ -56,8 +64,10 @@ export class Card {
     interval: number;
     cumulativeInterval: number;
     grade: number;
+    last_review: Date;
+    retrievability: number[] = []
 
-    public constructor(state: number, difficulty: number, displayDifficulty: number, stability: number, interval: number, cumulativeInterval: number, grade: number) {
+    public constructor(state: number, difficulty: number, displayDifficulty: number, stability: number, interval: number, cumulativeInterval: number, grade: number, last_review: Date) {
         this.state = state;
         this.difficulty = difficulty;
         this.displayDifficulty = displayDifficulty;
@@ -65,5 +75,6 @@ export class Card {
         this.interval = interval;
         this.cumulativeInterval = cumulativeInterval;
         this.grade = grade;
+        this.last_review = last_review;
     }
 }
